11e72f939137403a39a85e0982f8d2c02336c8db
leberman <leberman11@gmail.com>
2022-08-11 20:22:09 +0430

    Data Modeling for User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class () extends Migration {
         });
     }
 
-    public function down():void
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
